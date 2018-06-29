df8333a8d6efb66e03c6f6fe7b5412cf430fbedb
Jessica Yohana Otto <jessicayohanaotto@gmail.com>
2018-06-29 14:57:39 -0300

    Add files via upload

---
+++ redes_prog/programacao.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aluno
+ * Date: 29/06/18
+ * Time: 14:20
+ */
+/*
+$mascara = $_GET["mascara"];
+$parte4=$_GET["parte4"];
+$bits = 32 - $mascara;
+
+        $mascaradec = 256 - pow(2,$bits);
+
+professor
+
+*/
